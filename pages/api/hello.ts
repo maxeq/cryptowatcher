d240bc2bae46e86eba0b42418c76bc11b205357d
maxeq <maxeq1@gmail.com>
2023-03-06 19:42:54 +0100

    db func update

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import dbSaveData from '@/database/dbSaveData'
 
 type Data = {
   name: string
