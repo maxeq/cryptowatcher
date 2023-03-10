3d826d1940022b66f2ce8839d8f3b6f61e47a32f
maxeq <maxeq1@gmail.com>
2023-03-10 13:18:16 +0100

    Update Crypto_table.tsx

--- components/Crypto_table.tsx
+++ components/Crypto_table.tsx
@@ -4,7 +4,7 @@ import useSWRInfinite from "swr/infinite";
 import Loader from "./Loader";
 
 const fetcher = (url: any) => fetch(url).then((res) => res.json());
-const PAGE_SIZE = 8;
+const PAGE_SIZE = 2;
 
 export default function CoinList() {
     const [currentPage, setCurrentPage] = useState(1);
@@ -26,9 +26,10 @@ export default function CoinList() {
 
     return (
         <div>
+            {Error() && <div>Failed to load</div>}
             <InfiniteScroll next={() => setSize(size + 1)} 
             hasMore={!isReachedEnd} 
-            loader={<Loader />} 
+            loader='loading'
             endMessage={<p>Reached end</p>} 
             dataLength={data?.length ?? 0}>
 
