6d1b6d1f0e3b4efb1883e49b58fb9758b44c638e
maxeq <maxeq1@gmail.com>
2023-03-10 12:41:22 +0100

    Update Crypto_table.tsx

--- components/Crypto_table.tsx
+++ components/Crypto_table.tsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 import useSWRInfinite from "swr/infinite";
+import Loader from "./Loader";
 
 const fetcher = (url: any) => fetch(url).then((res) => res.json());
-const PAGE_SIZE = 6;
+const PAGE_SIZE = 10;
 
 export default function CoinList() {
   const [currentPage, setCurrentPage] = useState(1);
 
   const getKey = (pageIndex: number, previousPageData: any) => {
-    return `/api/crypto/getData?page=${pageIndex + 1}&pageSize=${PAGE_SIZE}`;
+    pageIndex = pageIndex + 1;
+    if (previousPageData && !previousPageData.getdata.length) return null;
+
+    return `/api/crypto/getData?page=${pageIndex}&pageSize=${PAGE_SIZE}`;
   };
   
   const { data, size, setSize } = useSWRInfinite(getKey, fetcher);
+
+  const isReachedEnd = !data || data[data.length - 1].getdata.length < PAGE_SIZE;
+
+  const loadingMore = size > 0 && data && typeof data[size - 1] === "undefined";
 
   console.log(data);
 
   return (
     <div>
       {data && data.map((page, index) => (
+        
         <div key={index}>
           {page.getdata.map((item: any) => (
             <div key={item.id}>
@@ -28,12 +37,13 @@ export default function CoinList() {
           ))}
         </div>
       ))}
-      <button
+      {loadingMore && <Loader />}
+      {!isReachedEnd && <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={() => setSize(size + 1)}
       >
         Load more
-      </button>
+      </button>}
     </div>
   );
 }
