c6511f1389a28ec7a3b672cb1dd6991339145eb3
maxeq <maxeq1@gmail.com>
2023-03-10 11:52:03 +0100

    Update Ticker.tsx

--- components/Ticker.tsx
+++ components/Ticker.tsx
@@ -36,38 +36,10 @@ export default function Ticker({ pageName }: TickerProps): JSX.Element {
   const cryptocurrencies = useCoingeckoTicker();
 
   return (
-    <>
-      {pageName === "Crypto" ? (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {cryptocurrencies.map((crypto: CryptoProps["crypto"]) => (
             <Crypto key={crypto.id} crypto={crypto} />
           ))}
         </div>
-      ) : pageName === "Crypto_table" ? (
-        <div className="relative overflow-x-auto">
-          <table className={`table mx-auto`}>
-            <thead>
-              <tr className={`table-header`}>
-                <th className="table__start sticky z-0 backdrop-opacity-2 max-sm:bg-gray-800">#</th>
-                <th className="table__start sticky z-0 backdrop-opacity-0 max-sm:bg-gray-800">Cryptocurrency</th>
-                <th className="table__end">Price (USD)</th>
-                <th className="table__end">1h %</th>
-                <th className="table__end">24h %</th>
-                <th className="table__end">7d %</th>
-                <th className="table__end">Market Cap</th>
-                <th className="table__end">Volume (24h)</th>
-                <th className="table__end">Circulating Supply</th>
-                <th className="table__end">Live data</th>
-              </tr>
-            </thead>
-            <tbody>
-              {cryptocurrencies.map((crypto: CryptoProps["crypto"], index: number) => (
-                <Crypto_table key={crypto.id} index={index} crypto={crypto} />
-              ))}
-            </tbody>
-          </table>
-        </div>
-      ) : null}
-    </>
   );
 }
