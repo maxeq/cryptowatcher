2984e8c155d81f64a3994d08d8906faa45985b14
maxeq <maxeq1@gmail.com>
2023-03-12 21:04:31 +0100

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import H1Template from '@/components/h1template'
+
+const Error = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/");
+        }, 3000);
+    }, []);
+
+    return (
+        <div>
+            <H1Template text="Eror 404. Page not found" />
+            <p>Page not found. Redirecting to main page.</p>
+        </div>
+    );
+};
+
+export default Error;
