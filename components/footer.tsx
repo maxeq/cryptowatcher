6f9df085be8b5e3f59444cdee8164755333b0027
maxeq <maxeq1@gmail.com>
2023-03-12 21:28:34 +0100

    u

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import Logo from "../components/logo";
+import Logo from "./logo";
+
 export default function Footer() {
     return (
         <footer className="bg-gray-900 text-white flex shadow py-8">
--- components/logo.tsx
+++ components/logo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 function Logo() {
   const handleTelegram = () => {
-    window.open('https://t.me/more_media');
+    window.open('https://t.me/more_media', '_blank');
   };
   return <Image src="/logo.png" alt="logo" width="128" height="128" onClick={handleTelegram} className="hover:animate-ping"/>
 }
