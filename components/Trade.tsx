6180d3d69e6fbf1034590a4c1329f49f282f9265
maxeq <maxeq1@gmail.com>
2023-03-12 19:21:22 +0100

    Create Trade.tsx

---
+++ components/Trade.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+
+export default function Trade() {
+  return (
+    <div>
+      <Head>
+        <title>CryptoWatcher Trade</title>
+        <meta name="description" content="CryptoWatcher Trade" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div className="flex">
+          <div className="w-3/5">
+            <div className="flex justify-between">
+              <div>
+                BNB/ETH
+              </div>
+              <div className="">Expand</div>
+            </div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="w-2/5">
+            Trade
+            <div className="bg-white rounded-lg shadow-lg p-80"></div>
+          </div>
+        </div>
+      </main>
+
+    </div>
+
+  )
+}
