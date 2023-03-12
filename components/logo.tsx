a873679798995177ca36d7457c1c87517b9ca343
maxeq <maxeq1@gmail.com>
2023-03-12 21:30:09 +0100

    Update logo.tsx

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ function Logo() {
   const handleTelegram = () => {
     window.open('https://t.me/more_media', '_blank');
   };
-  return <Image src="/logo.png" alt="logo" width="128" height="128" onClick={handleTelegram} className="hover:animate-ping"/>
+  return <Image src="/logo.png" alt="logo" width="128" height="128" onClick={handleTelegram} className="hover:animate-ping" placeholder='blur'/>
 }
 
 export default Logo
