3e6b00c438408e70788033f8db49b96af59718ac
maxeq <maxeq1@gmail.com>
2023-03-14 17:03:12 +0100

    y

--- pages/cryptocurrencies/[ids].tsx
+++ pages/cryptocurrencies/[ids].tsx
@@ -1,132 +1,20 @@
 import React from "react";
-import { useRouter } from 'next/router';
-import useSWR from 'swr';
-import Image from 'next/image';
-import { formatPrice, formatPercent } from "@/utils";
+import type { NextPage } from "next";
+import Head from "next/head";
 
-import { FaShareSquare, FaRegStar } from 'react-icons/fa';
+import NoSSR from "../../components/NoSSR";
+import Dashboard from "@/components/Dashboard";
 
-const fetcher = async (url: string) => {
-    const res = await fetch(url);
-    const data = await res.json();
-    return data;
-}
-
-export default function DashboardCrypto(): JSX.Element {
-    const router = useRouter();
-    const ids = Array.isArray(router.query.ids) ? router.query.ids[0] : router.query.ids;
-    const { data: cryptocurrencies, error } = useSWR(
-        `/api/crypto/getCryptoData?id=${ids}`,
-        fetcher,
-        {
-            shouldRetryOnError: false,
-            onSuccess: (data) => {
-                return data || [];
-            },
-            onError: (error) => {
-                console.error(error);
-            },
-            revalidateOnMount: true,
-            dedupingInterval: 5000 // Cache for 5 minutes
-        }
-    );
-
-    const classPriceChangePercent = (value: any) =>
-        value
-            ? value === "0%"
-                ? "text-white-300"
-                : value < 0
-                    ? "bg-rose-600"
-                    : "bg-lime-600"
-            : "bg-white-300";
-
-
-    if (error) return <div>Error fetching data</div>;
-    if (!cryptocurrencies || !cryptocurrencies.getdata) return <div>Loading data...</div>;
-
-    const { ath, atl, max_supply, total_volume, circulating_supply, market_cap_change_24h, total_supply, price_change_percentage_24h, current_price, high_24h, id, image, low_24h, market_cap, market_cap_rank, name, symbol, fully_diluted_valuation } = cryptocurrencies.getdata;
-
+const Cryptocurrencies_dynamic: NextPage = () => {
     return (
-        <div key={ids}>
-            <div className="md:p-8 p-4 shadow-purple-400 bg-gray-900 border border-solid border-gray-100 rounded-lg shadow-lg">
-                <div className="md:flex md:justify-between">
-                    <div className="">
-                        <div className="">
-                            <div className="flex md:justify-between items-center">
-                                <Image src={image} alt={name} width="64" height="64" className="mr-2" />
-                                <div className="text-5xl md:ml-3">{name}</div>
-                                <div className="flex pt-4 space-x-3 ml-3">
-                                    <div className="border-2 border-gray-500 text-gray-300 rounded-lg p-1 hover:text-gray-200"><FaRegStar size="24" /></div>
-                                    <div className="border-2 border-gray-500 text-gray-300 rounded-lg p-1 hover:text-gray-200"><FaShareSquare size="24" /></div>
-                                </div>
-                            </div>
-                            <div className="flex space-x-3 text-sm mt-4 text-slate-300">
-                                <div className="border-b-gray-500 border text-gray-100 bg-gray-800 rounded px-2">Rank #{market_cap_rank}</div>
-                                <div className="border-b-gray-500 border bg-gray-800 rounded px-2">Coin</div>
-                                <div className="border-b-gray-500 border bg-gray-800 rounded px-2">Followed by {12}</div>
-                            </div>
-                            <div className="text-lg mt-4">
-                                <div className="mt-4">
-                                    <div className="text-sm text-slate-300">Volume</div>
-                                    <div className="text-xl">{formatPrice(total_volume)}</div>
-                                </div>
-                                <div className="mt-4">
-                                    <div className="text-sm text-slate-300">Fully Diluted Valuation</div>
-                                    <div className="text-xl">{formatPrice(fully_diluted_valuation)}</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <NoSSR>
+            <Head>
+                <title>Cryptocurrencies</title>
+            </Head>
+            <Dashboard />
+            </NoSSR>
+    );
+};
 
-                    <div className="">
-                        <div className="pt-2">
-                            <div className="text-slate-300 text-xs justify-center lg:flex lg:justify-center">{name} Price ({symbol.toUpperCase()})</div>
-                            <div className="flex items-center space-x-2 mt-2 lg:flex lg:justify-center">
-                                <div className="text-6xl font-bold">{formatPrice(current_price)}</div>
-                                <div className="lg:px-2">
-                                    <div className="text-center text-slate-300 text-lg">24h</div>
-                                    <div className={`text-base md:text-xl text-white rounded-lg px-2 ${classPriceChangePercent(price_change_percentage_24h)}`}>{formatPercent(price_change_percentage_24h)}</div>
-                                </div>
-                            </div>
-                            <div className="flex justify-between mt-4">
-                                <div className="text-lg">Low: {formatPrice(low_24h)}</div>
-                                <div className="text-lg">High: {formatPrice(high_24h)}</div>
-                            </div>
-                        </div>
-                        <div className="md:flex md:justify-between border-t-2 mt-4 border-t-purple-400 py-4">
-                            <div className="mr-20">
-                                <div className="text-slate-300 text-xs">MarketCap</div>
-                                <div className="text-xl">{formatPrice(market_cap)}</div>
-                            </div>
-                            <div>
-                                <div className="text-slate-300 text-xs pt-4 md:pt-0">Market Cap Change 24h</div>
-                                <div className="text-xl">{formatPrice(market_cap_change_24h)}</div>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div className="">
-                        <div className="mt-4">
-                            <div className="text-sm text-slate-300">Circulating Supply</div>
-                            <div className="text-xl">{formatPrice(circulating_supply).replace("$", "")}</div>
-                        </div>
-                        <div className="mt-4 space-y-2">
-                            <div className="mt-4">
-                                <div className="text-sm text-slate-300">Max Supply</div>
-                                <div className="text-xl">{formatPrice(max_supply).replace("$", "")}</div>
-                            </div>
-                            <div className="">
-                                <div className="text-sm text-slate-300">Total Supply</div>
-                                <div className="text-xl">{formatPrice(total_supply).replace("$", "")}</div>
-                            </div>
-                        </div>
-                        <div className="flex justify-center">
-                            <button className="px-12 mt-6 bg-lime-600 hover:bg-lime-500 shadow-lg shadow-lime-500/50 py-2 mx:px-0 text-white font-bold rounded whitespace-nowrap">Buy {name}</button>
-                        </div>
-                    </div>
-                </div>
-        </div> 
-        </div> 
- 
-    )
-} 
+export default Cryptocurrencies_dynamic;
