1f5108472b564f2c3aeb11fb73b04dd1a486eb14
maxeq <maxeq1@gmail.com>
2023-03-14 16:52:17 +0100

    Update [ids].tsx

--- pages/cryptocurrencies/[ids].tsx
+++ pages/cryptocurrencies/[ids].tsx
@@ -121,7 +121,7 @@ export default function DashboardCrypto(): JSX.Element {
                             </div>
                         </div>
                         <div className="flex justify-center">
-                            <button className="px-12 mt-6 bg-lime-600 hover:bg-lime-500 shadow-lg shadow-lime-500/50 py-2 mx:px-0 text-white font-bold rounded whitespace-nowrap">Buy</button>
+                            <button className="px-12 mt-6 bg-lime-600 hover:bg-lime-500 shadow-lg shadow-lime-500/50 py-2 mx:px-0 text-white font-bold rounded whitespace-nowrap">Buy {name}</button>
                         </div>
                     </div>
                 </div>
