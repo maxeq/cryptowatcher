0bc8e3dda3635676151f68b63fd3138218917f78
maxeq <maxeq1@gmail.com>
2023-03-15 13:24:16 +0100

    format price fixed

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,6 @@
 function formatPrice(price = 0) {
   const formattedPrice = Math.round(Number(price) * 100) / 100;
-  return `$${formattedPrice > 0 ? formattedPrice.toLocaleString() : price}`;
+  return `$${formattedPrice.toLocaleString()}`;
 }
 
 function formatPercent(percent) {
