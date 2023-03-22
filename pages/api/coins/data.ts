5d6c33f57f2fdbde3b3094be8b0771efb7764362
maxeq <maxeq1@gmail.com>
2023-03-22 21:04:11 +0100

    db

--- pages/api/coins/data.ts
+++ pages/api/coins/data.ts
@@ -2,25 +2,29 @@ import clientPromise from '../../../lib/mongodb';
 import { NextApiRequest, NextApiResponse } from 'next/types';
 import { CoinData } from '../../../types/coins/coindata';
 
-const getDataForPast10Mins = async (): Promise<CoinData[]> => {
+const getData = async (param: string, value: string): Promise<CoinData[]> => {
     const mongoClient = await clientPromise;
-    const currentTime = new Date();
-    const oneHour = new Date(currentTime.getTime() - 60 * 60 * 1000);
+
+    const query = value ? { [param]: value } : {};
 
     const data = await mongoClient
         .db('myFirstDatabase')
         .collection('coins')
-        .find({ dbDateAdded: { $gte: oneHour } })
+        .find(query)
+        .sort({ dbDateAdded: -1 })
+        .limit(3)
         .toArray();
 
     return JSON.parse(JSON.stringify(data));
 };
 
+
 const handler = async (
     req: NextApiRequest,
     res: NextApiResponse<{ getdata: CoinData[] }>
 ) => {
-    const data = await getDataForPast10Mins();
+    const { param } = req.query;
+    const data = await getData(param as string, '');
     res.status(200).json({ getdata: data });
 };
 
