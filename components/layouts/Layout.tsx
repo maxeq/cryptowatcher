31ebf7527d7f82ba79d418807487969e16a26f05
maxeq <maxeq1@gmail.com>
2023-03-24 00:25:13 +0100

    Update Layout.tsx

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -9,10 +9,10 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="min-h-screen">
       <Meta />
       <Header />
-      <div className="mx-auto max-w-[1440px] px-2 sm:py-2 md:py-4 lg:py-10 flex-grow">
+      <div className="mx-auto max-w-7xl px-2 sm:py-2 md:py-4 lg:py-10">
         <main className="container mx-auto">
           {children}
         </main>
