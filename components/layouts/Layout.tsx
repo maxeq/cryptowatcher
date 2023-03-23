e38a21e04d0eb638de22cdc9183868eb65b1208b
maxeq <maxeq1@gmail.com>
2023-03-24 00:28:09 +0100

    Update Layout.tsx

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -3,6 +3,7 @@ import Meta from './Meta';
 import Header from '../../components/nav/HeaderNav';
 import Footer from '../../components/nav/FooterNav';
 
+
 interface LayoutProps {
   children: ReactNode;
 }
@@ -12,7 +13,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="min-h-screen">
       <Meta />
       <Header />
-      <div className="mx-auto max-w-7xl px-2 sm:py-2 md:py-4 lg:py-10">
+      <div className="mx-auto max-w-[1440px] px-2 sm:py-2 md:py-4 lg:py-10">
         <main className="container mx-auto">
           {children}
         </main>
