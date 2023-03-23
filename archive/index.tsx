5157847812488da8b285d80a4c8335a04e4ba7a6
maxeq <maxeq1@gmail.com>
2023-03-23 15:01:52 +0100

    archive

--- archive/index.tsx
+++ archive/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import type { NextPage } from 'next';
 
-import H1Template from '@/components/UI/HeaderTemplate';
-import Head from 'next/head';
-import CryptoCards from '@/components/CryptoCards';
+// import H1Template from '@/components/UI/HeaderTemplate';
+// import Head from 'next/head';
+// import CryptoCards from '@/components/CryptoCards';
 
-const Home: NextPage = () => {
+const ArchHome: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <H1Template text="CryptoWatcher never sleeps. Built by diamond hands." />
-      <CryptoCards />
+      {/* //       <Head>
+//         <title>Home</title>
+//       </Head>
+//       <H1Template text="CryptoWatcher never sleeps. Built by diamond hands." />
+//       <CryptoCards /> */}
     </>
   );
 };
 
-export default Home;
+export default ArchHome;
