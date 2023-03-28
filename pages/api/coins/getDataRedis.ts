fa28bfe7efe5207f71529427d74a0ed534e600c4
maxeq <maxeq1@gmail.com>
2023-03-28 21:59:45 +0200

    Update getDataRedis.ts

--- pages/api/coins/getDataRedis.ts
+++ pages/api/coins/getDataRedis.ts
@@ -4,9 +4,8 @@ import { CoinData } from '../../../types/coins/coindata';
 import clientPromise from '../../../lib/mongodb';
 import Redis from 'ioredis';
 
-const redis = new Redis(process.env.REDIS_URL as string);
-
-const cacheAllData = async (): Promise<void> => {
+const cacheAllData = async (): Promise<string> => {
+    const redis = new Redis(process.env.REDIS_URL as string);
     const mongoClient: MongoClient = await clientPromise;
 
     const allData = await mongoClient
@@ -27,6 +26,7 @@ const cacheAllData = async (): Promise<void> => {
 
     await redis.set('allData', JSON.stringify(allData), 'EX', 5 * 60);
     redis.disconnect();
+    return JSON.stringify(allData);
 };
 
 const sortData = (data: CoinData[], sortKey: string, sortDirection: string): CoinData[] => {
@@ -34,17 +34,14 @@ const sortData = (data: CoinData[], sortKey: string, sortDirection: string): Coi
         const key = sortKey as keyof CoinData;
         const aValue = a[key];
         const bValue = b[key];
-
         if (typeof aValue === 'number' && typeof bValue === 'number') {
             return sortDirection === 'asc' ? aValue - bValue : bValue - aValue;
         } else if (typeof aValue === 'string' && typeof bValue === 'string') {
             return sortDirection === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
         }
-
         return 0;
     });
 };
-
 
 const getData = async (
     page: number,
@@ -52,14 +49,16 @@ const getData = async (
     sortKey: string,
     sortDirection: string
 ): Promise<CoinData[]> => {
-    const cacheData = await redis.get('allData');
+    const redis = new Redis(process.env.REDIS_URL as string);
+    let cacheData = await redis.get('allData');
+    redis.disconnect();
 
     if (!cacheData) {
         // If the requested data is not in Redis, cache all data
-        await cacheAllData();
+        cacheData = await cacheAllData();
     }
 
-    const allData = JSON.parse(await redis.get('allData') || '[]');
+    const allData = JSON.parse(cacheData || '[]');
     const sortedData = sortData(allData, sortKey, sortDirection);
     const start = (page - 1) * pageSize;
     const end = start + pageSize;
