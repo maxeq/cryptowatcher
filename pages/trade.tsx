876062b2294c38636c48fbafdc8d0ded58c3a29e
maxeq <maxeq1@gmail.com>
2023-04-05 10:47:53 +0200

    s

--- pages/trade.tsx
+++ pages/trade.tsx
@@ -1,3 +1,4 @@
+import SliderData from '@/components/sliders/SliderData';
 import H1Template from '@/components/UI/HeaderTemplate';
 import TableComponent from '@/components/UI/table';
 import Head from 'next/head';
@@ -9,8 +10,18 @@ export default function Trade() {
         <title>Trending Widgets</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <H1Template text="Trending Widgets" className="mt-3 md:mt-0" />
+      <H1Template text="Window Scrolling Example" className="mt-4 md:mt-0" />
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        <div className="hidden sm:block">
+          <SliderData sliderTypes={['trending']} />
+        </div>
+        <div className="hidden sm:block">
+          <SliderData sliderTypes={['biggestLosers']} />
+        </div>
+        <div className="hidden sm:block">
+          <SliderData sliderTypes={['topGainers']} />
+        </div>
+      </div>
       <TableComponent />
     </div>
   );
